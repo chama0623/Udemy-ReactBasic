0ec4d626e5f0303e97835a41101f485c581075ac
ちゃま <49825985+yudai0731@users.noreply.github.com>
2022-12-07 08:44:02 +0900

    add useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 // コンポーネント
 // コンポーネント名はキャメルケース
 // {}内はJSとして認識される
 const App = () => {
-  const onClickButton = () => alert();
+  // ボタンが押されたときにstateを更新する
+  const onClickCountUp = () => {
+    // useStateの第2引数で指定した関数に値をセットすることでnumの値が変化する
+    setNum(num + 1);
+  };
+  // [stateに使用する変数名(自由), stateを変更する関数名]
+  const [num, setNum] = useState(0); // 初期値を0として与える
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちわ!</h1>
@@ -13,7 +20,8 @@ const App = () => {
         元気があればなんでもできる！
       </ColorfulMessage>
       <ColorfulMessage color="pink">1, 2, 3 ダー!</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
